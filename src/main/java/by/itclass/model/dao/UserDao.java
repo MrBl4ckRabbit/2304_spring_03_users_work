2ad58196d6f6c8dc0c78db9a4446598850414954
MrBlackRabbit <mr.bl4ck.rabbit@gmail.com>
2024-05-17 21:27:31 +0300

    change

--- src/main/java/by/itclass/model/dao/UserDao.java
+++ src/main/java/by/itclass/model/dao/UserDao.java
@@ -1,2 +1,10 @@
-package by.itclass.model.dao;public interface UserDao {
+package by.itclass.model.dao;
+
+import by.itclass.model.entities.User;
+
+import java.util.List;
+
+public interface UserDao {
+    List<User> selectAllUsers();
+
 }
--- src/main/java/by/itclass/model/dao/impl/UserDaoImpl.java
+++ src/main/java/by/itclass/model/dao/impl/UserDaoImpl.java
@@ -1,15 +1,27 @@
-package by.itclass.model.impl;
+package by.itclass.model.dao.impl;
 
 import by.itclass.model.dao.UserDao;
 import by.itclass.model.entities.User;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 public class UserDaoImpl implements UserDao {
+    private SessionFactory sessionFactory;
+
+    @Autowired
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
     @Override
     public List<User> selectAllUsers() {
-        return List.of();
+
+        try (var session = sessionFactory.openSession()) {
+            return session.createQuery("from User", User.class).list();
+        }
     }
 }
